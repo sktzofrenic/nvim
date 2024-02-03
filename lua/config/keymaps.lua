a39c95b4bfbd5ed5bc7fa8d9336796271b50431d
Dan Safee <dan@getdrx.com>
2024-02-02 21:05:18 -0500

    Add copilot mapping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -22,3 +22,8 @@ vim.api.nvim_set_keymap("n", "<C-q>", ":cope<CR>", { noremap = true, silent = tr
 vim.api.nvim_set_keymap("n", "n", "nzz", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "N", "Nzz", { noremap = true, silent = true })
 
+vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+    expr = true,
+    replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
