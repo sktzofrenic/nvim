5f4eb62e129aeed735871ba0216677e68035470a
Dan Safee <dan@getdrx.com>
2024-02-23 07:11:03 -0500

    Add more key maps for telescope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,6 +16,7 @@ vim.api.nvim_set_keymap(
 )
 vim.api.nvim_set_keymap("n", "<Leader>gh", ":DiffviewFileHistory %<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>sl", ":Telescope resume<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>hh", ":Telescope find_files hidden=true no_ignore=true<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "]g", ":cn<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "[g", ":cp<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>h", ":nohl<CR>", { noremap = true, silent = true })
