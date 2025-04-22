46336d7d0d8b948528ffbf451851b508fecf4305
Dan Safee <dan@getdrx.com>
2025-04-22 09:15:27 -0400

    Add options for neovide config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,4 +11,9 @@ vim.g.root_spec = {
     { ".git", 'lua'},
     'cwd'
 }
-
+vim.opt.guifont = {
+    "FiraCode Nerd Font", ":h12",
+}
+vim.api.nvim_set_keymap("v", "<sc-c>", '"+y', { noremap = true }) -- Select line(s) in visual mode and copy (CTRL+Shift+V)
+vim.api.nvim_set_keymap("i", "<sc-v>", '<ESC>"+p', { noremap = true }) -- Paste in insert mode (CTRL+Shift+C)
+vim.api.nvim_set_keymap("n", "<sc-v>", '"+p', { noremap = true }) -- Paste in normal mode (CTRL+Shift+C)
