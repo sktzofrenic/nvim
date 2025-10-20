f66c2e6fa81b929710fb3e0cc26b19a072600a3b
Dan Safee <dan@danwins.com>
2025-10-20 16:04:28 -0400

    Set leg level off

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -17,3 +17,5 @@ vim.opt.guifont = {
 vim.api.nvim_set_keymap("v", "<sc-c>", '"+y', { noremap = true }) -- Select line(s) in visual mode and copy (CTRL+Shift+V)
 vim.api.nvim_set_keymap("i", "<sc-v>", '<ESC>"+p', { noremap = true }) -- Paste in insert mode (CTRL+Shift+C)
 vim.api.nvim_set_keymap("n", "<sc-v>", '"+p', { noremap = true }) -- Paste in normal mode (CTRL+Shift+C)
+
+vim.lsp.log.set_level("off")
