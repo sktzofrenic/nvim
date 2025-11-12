5751ffa7b7ee2420a84882ee6eb2003ce26fb6c6
Dan Safee <dan@danwins.com>
2025-11-12 12:06:53 -0500

    Update to latest

--- lua/plugins/projects.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        'ahmedkhalf/project.nvim',
-        opts = {
-            patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile"},
-            detection_methods = { "pattern" },
-        },
-    }
-}
