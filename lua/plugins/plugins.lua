fff7e85b4b14b55182dc283899f4643d23b3d8cc
Dan Safee <dan@danwins.com>
2025-11-12 11:22:43 -0500

    Remove telescope thing

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,20 +1,4 @@
 return {
-    {
-        "ahmedkhalf/project.nvim",
-        opts = {
-            manual_mode = false,
-        },
-        event = "VeryLazy",
-        config = function(_, opts)
-            require("project_nvim").setup(opts)
-            require("lazyvim.util").on_load("telescope.nvim", function()
-                require("telescope").load_extension("projects")
-            end)
-        end,
-        keys = {
-            { "<leader>fp", "<Cmd>Telescope projects<CR>", desc = "Projects" },
-        },
-    },
     { "mbbill/undotree" },
     {
         "cbochs/grapple.nvim",
