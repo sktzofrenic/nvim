44db133217a81f23bd3ccf7837eb5d3076c6a8f6
Dan Safee <dan@danwins.com>
2025-11-24 11:31:05 -0500

    Update to latest

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,6 @@
 return {
     {
-        "navarasu/onedark.nvim",
+        "olimorris/onedarkpro.nvim",
         priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            require('onedark').setup {
-                style = 'deep'
-            }
-            require('onedark').load()
-        end
     }
 }
